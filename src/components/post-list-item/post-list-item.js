f97d79d6ac63bd59def87d1d259469b69dfdfe2f
bek-app <make202006@gmail.com>
2021-06-08 16:03:44 +0600

    onImportant

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
 import "./post-list-item.css";
 export default class PostListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            important: false,
+        };
+        this.onImportant = this.onImportant.bind(this);
+    }
+    onImportant() {
+        this.setState(({ important }) => ({
+            important: !important,
+        }));
+    }
     render() {
-        const { label, important = false } = this.props;
+        const { label } = this.props;
+        const { important } = this.state;
         let classNames = "app-list-item d-flex justify-content-between";
         if (important) {
             classNames += " important";
@@ -12,7 +25,11 @@ export default class PostListItem extends Component {
             <div className={classNames}>
                 <span className="app-list-item-label"> {label}</span>
                 <div className="d-flex justify-content-center align-items-center">
-                    <button type="button" className="btn-star btn-sm">
+                    <button
+                        type="button"
+                        className="btn-star btn-sm"
+                        onClick={this.onImportant}
+                    >
                         {" "}
                         <i className="fa fa-star"></i>
                     </button>
